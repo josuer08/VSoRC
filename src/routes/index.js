7bfd7771a3323b0b65d8ad7d7a3f3bba24373270
VSORC <josuer08@gmail.com>
2019-10-08 15:35:03 -0400

    installed websocket module (unnecesary) and mod

--- src/routes/index.js
+++ src/routes/index.js
@@ -96,48 +96,10 @@ router.get('/startvsorc', (req, res) => {
   var sys = require('sys')
   var exec = require('child_process').exec;
   var child;
-  child = exec("setsid /home/pi/lol.py", function(error, stdout, stderr) {
+  child = exec("cd /home/pi && cat fifo | sudo ./clusterGRE.py > aichivo 2>&1 ", function(error, stdout, stderr) {
     console.log(stdout);
     res.send(stdout);
   });
 });
-
-
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
-});
-server.listen(1337, function() { });
-
-// create the server
-wsServer = new WebSocketServer({
-  httpServer: server
-});
-
-// WebSocket server
-wsServer.on('request', function(request) {
-  var connection = request.accept(null, request.origin);
-  console.log('Se conecto '+request.origin);
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      // process WebSocket message
-      console.log('Recibido');
-      console.log(message);
-      connection.sendUTF("hola");
-
-    }
-  });
-
-  connection.on('close', function(connection) {
-    // close user connection
-  });
-});
-
 
 module.exports = router;
